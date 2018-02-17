d3d4ccab6e49b9438ae9ebe1e452407e77d33090
Alexander Slesarenko <avslesarenko@gmail.com>
2018-02-17 11:09:06 +0300

    simple examples from waves test suite

--- smart-api/src/main/scala/smart/Primitives.scala
+++ smart-api/src/main/scala/smart/Primitives.scala
@@ -4,13 +4,13 @@ import scodec.bits.ByteVector
 
 import scalan.Entity
 
-trait SmartType { type Underlying }
-trait NOTHING              extends SmartType { type Underlying = Nothing         }
-trait UNIT                 extends SmartType { type Underlying = Unit            }
-trait INT                  extends SmartType { type Underlying = Int                  }
-trait BYTEVECTOR           extends SmartType { type Underlying = ByteVector           }
-trait BOOLEAN              extends SmartType { type Underlying = Boolean              }
-trait OPTION[T <: SmartType] extends SmartType { type Underlying = Option[T#Underlying] }
+//trait SmartType { type Underlying }
+//trait NOTHING              extends SmartType { type Underlying = Nothing         }
+//trait UNIT                 extends SmartType { type Underlying = Unit            }
+//trait INT                  extends SmartType { type Underlying = Int                  }
+//trait BYTEVECTOR           extends SmartType { type Underlying = ByteVector           }
+//trait BOOLEAN              extends SmartType { type Underlying = Boolean              }
+//trait OPTION[T <: SmartType] extends SmartType { type Underlying = Option[T#Underlying] }
 
 @Entity
 trait Transaction {
@@ -34,9 +34,110 @@ trait Example[R] {
   def result: R
 }
 
-trait Example1 extends Example[Int] {
-  val X = 2
-  def result = 3
+class Ex1 extends Example[Int] {
+  def result = {
+    val x = 2
+    3
+  }
+//  expr = Block(
+//    Some(LET("x", CONST_INT(3))),
+//    CONST_INT(3)
+//  )
+}
+
+class Ex2 extends Example[Int] {
+  def result = {
+    val x = 3
+    val y = x
+    x + y
+  }
+  //      expr = Block(Some(LET("x", CONST_INT(3))),
+  //        Block(
+  //          Some(LET("y", REF("x"))),
+  //          SUM(REF("x"), REF("y"))
+  //        ))
+}
+
+class Ex3(i: Int) extends Example[Int] {
+  def result = {
+    val x = i
+    x
+  }
+  // Block(Some(LET("x", CONST_INT(i))), REF("x"))
+}
+
+class Ex4 extends Example[Boolean] {
+  def result = {
+    val x = 3
+    x == 2
+  }
+//  expr = Block(
+//    Some(LET("x", CONST_INT(3))),
+//    EQ(REF("x"), CONST_INT(2))
+//  )
+}
+
+class Ex5 extends Example[Boolean] {
+  def result = {
+    val x = 3
+    val y = 3
+    x == y
+  }
+//  expr = Block(
+//    Some(LET("x", CONST_INT(3))),
+//    Block(Some(LET("y", CONST_INT(3))), EQ(REF("x"), REF("y")))
+//  )
+}
+class Ex6 extends Example[Boolean] {
+  def result = {
+    val x = 3
+    val y = 3 + 0
+    x == y
+  }
+  //      expr = Block(
+  //        Some(LET("x", CONST_INT(3))),
+  //        Block(Some(LET("y", SUM(CONST_INT(3), CONST_INT(0)))), EQ(REF("x"), REF("y")))
+  //      )
+}
+class Ex7 extends Example[Int] {
+  def result = {
+    if (1 == 2) { new Ex3(3).result } else 4
+  }
+//  expr = IF(EQ(CONST_INT(1), CONST_INT(2)), simpleDeclarationAndUsage(3), CONST_INT(4))
+}
+
+class Ex8 extends Example[Int] {
+  def result = {
+    if (1 == 2) { new Ex3(3).result } else { new Ex3(4).result }
+  }
+//  expr = IF(EQ(CONST_INT(1), CONST_INT(2)), simpleDeclarationAndUsage(3), simpleDeclarationAndUsage(4))
+}
+
+class Ex10 extends Example[Boolean] {
+  def result = None.isDefined
+//  ev(expr = IS_DEFINED(NONE)) shouldBe Right(false)
+}
+
+class Ex11 extends Example[Boolean] {
+  def result = Some(1).isDefined
+//  ev(expr = IS_DEFINED(SOME(CONST_INT(1)))) shouldBe Right(true)
+}
+
+class Ex12 extends Example[Int] {
+  def result = Some(1).get
+//  ev(expr = GET(SOME(CONST_INT(1)))) shouldBe Right(1)
+}
+
+class Point(val X: Int, Y: Int)
+
+class Ex13 extends Example[Int] {
+  val p = new Point(X = 3, Y = 4)
+  def result = p.X + 2
+  //  ev(
+  //  predefTypes = Map(pointType.name -> pointType),
+  //  defs = Map("p" -> (TYPEREF("Point"), pointInstance)),
+  //  expr = SUM(GETTER(REF("p"), "X"), CONST_INT(2))
+  //  )
 }
 
 
--- smart-api/src/test/scala/smart/PrimitivesTest.scala
+++ smart-api/src/test/scala/smart/PrimitivesTest.scala
@@ -1,4 +1,53 @@
 package smart
 
-class PrimitivesTest extends BaseNestedTest {
+import org.scalameter.Context
+
+import scala.util.Try
+import scalan.BaseNestedTests
+
+class PrimitivesTest extends BaseNestedTests {
+  private def ev[T](e: Example[T]): Either[_, T] =
+    try { Right(e.result) } catch { case t: Throwable => Left(t) }
+
+  it("successful on unused let") {
+    ev(new Ex1) shouldBe Right(3)
+  }
+
+  it("successful on x = y") {
+    ev(new Ex2) shouldBe Right(6)
+  }
+
+  it("successful on simple get") {
+    ev(new Ex3(3)) shouldBe Right(3)
+  }
+
+  it("successful on get used further in expr") {
+    ev(new Ex4) shouldBe Right(false)
+  }
+
+  it("successful on multiple lets") {
+    ev(new Ex5) shouldBe Right(true)
+  }
+
+  it("successful on multiple lets with expression") {
+    ev(new Ex6) shouldBe Right(true)
+  }
+
+  it("successful on deep type resolution") {
+    ev(new Ex7) shouldBe Right(4)
+  }
+
+  it("successful on same value names in different branches") {
+    ev(new Ex8) shouldBe Right(4)
+  }
+
+  it("successful GET/IS_DEFINED") {
+    ev(new Ex10) shouldBe Right(false)
+    ev(new Ex11) shouldBe Right(true)
+    ev(new Ex12) shouldBe Right(1)
+  }
+
+  it("custom type field access") {
+    ev(new Ex13) shouldBe Right(5)
+  }
 }
